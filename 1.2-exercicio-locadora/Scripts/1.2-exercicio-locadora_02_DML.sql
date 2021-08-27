a649af51ae188528c95848b5e8bf3b25a07330f1
biancamarangoni <bchagas228@gmail.com>
2021-08-27 18:41:10 -0300

    finalizado

--- 1.2-exercicio-locadora/Scripts/1.2-exercicio-locadora_02_DML.sql
+++ 1.2-exercicio-locadora/Scripts/1.2-exercicio-locadora_02_DML.sql
@@ -10,21 +10,22 @@ INSERT INTO CLIENTE (nomeCliente, cpf)
 VALUES ('Antonio', '22222222222'), ('Laura', '66666666666');
 GO
 
+INSERT INTO MARCA (nomeMarca)
+VALUES (1, 'Fiat'), (2, 'GM'), (3, 'bmw');
+GO
+
 INSERT INTO MODELO (idMarca, nomeModelo)
 VALUES (1, 'toro'),  (2, 'ix35'), (3, 'X3');
 GO
 
-INSERT INTO MARCA (nomeMarca)
-VALUES (1, 'Fiat'), (2, 'GM'), (3, 'bmw');
-GO
 
 INSERT INTO VEICULO( idModelo, idEmpresa, idPlaca)
 VALUES (1, 1, 'ANL2225'), (2, 1, 'GJH1819'), ( 3, 2, 'GJH1819'), (1, 2, 'HRV8018')
 GO
 
 INSERT INTO ALUGUEL( idVeiculo, idCliente, dataRetirada, dataDevolucao)
-VALUES (1,  1 '12-03-2021', '20-03-2021'), 
-       (1,  2, '25-10-2021', '01-11-2021'),
+VALUES (1,  1 '17/08/2021', '19/08/2021'), 
+       (2,  1, '20/08/2021', '26/08/2021'),
 	   (3,  2, '22/08/2021', '30/08/2021'),
 	   (2,  2, '22/08/2021', '18/08/2021' );
 GO
